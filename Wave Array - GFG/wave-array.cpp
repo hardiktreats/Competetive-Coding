8a9420099786bd6751b673a5a5ab2107bcce6f24
Hardik Dhanjal <73985915+hardiktreats@users.noreply.github.com>
2022-02-24 19:51:47 +0530

    Added solution - LeetHub

---
+++ Wave Array - GFG/wave-array.cpp
@@ -0,0 +1,50 @@
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+class Solution{
+    public:
+    // arr: input array
+    // n: size of array
+    //Function to sort the array into a wave-like array.
+    void convertToWave(vector<int>& arr, int n){
+       int i;
+       if(n%2==0){
+        for(i=0;i<n;i+=2){
+            if(arr[i]<arr[i+1])
+                swap(arr[i],arr[i+1]);
+            }
+       }
+       else if(n%2!=0){
+           for(i=0;i<n-1;i+=2){
+       if(arr[i]<arr[i+1])
+            swap(arr[i],arr[i+1]);
+       }
+       }
+        
+    }
+};
+
+// { Driver Code Starts.
+
+int main()
+{
+    int t,n;
+    cin>>t; //Input testcases
+    while(t--) //While testcases exist
+    {
+        cin>>n; //input size of array
+        vector<int> a(n); //declare vector of size n
+        for(int i=0;i<n;i++)
+            cin>>a[i]; //input elements of array
+        Solution ob;
+        ob.convertToWave(a, n);
+
+        for(int i=0;i<n;i++)
+            cout<<a[i]<<" "; //print array
+            
+        cout<<endl;
+    }
+}  // } Driver Code Ends
