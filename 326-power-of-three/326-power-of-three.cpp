77db441f04f51c341f3e6ac06ff7eebe0644f674
Hardik Dhanjal <73985915+hardiktreats@users.noreply.github.com>
2022-08-24 09:13:19 +0530

    Time: 31 ms (61.16%), Space: 6.2 MB (7.72%) - LeetHub

--- 326-power-of-three/326-power-of-three.cpp
+++ 326-power-of-three/326-power-of-three.cpp
@@ -7,18 +7,3 @@ public:
         return (ceil(log10(n)/log10(3))==floor(log10(n)/log10(3)));
     }
 };
-
-/*class Solution {
-public:
-    bool isPowerOfThree(int n)
-    {
-      if(n==0)return false;
-
-      double a=log10(n)/log10(3);
-
-      if(ceil(a)==floor(a))
-          return true;
-
-      return false;
-    }
-};*/
