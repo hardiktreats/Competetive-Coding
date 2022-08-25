5805112adcfbc193da16eb6571231ddd07a6759e
Hardik Dhanjal <73985915+hardiktreats@users.noreply.github.com>
2022-08-25 11:09:44 +0530

    Time: 20 ms (69.06%), Space: 8.8 MB (39.49%) - LeetHub

---
+++ 383-ransom-note/383-ransom-note.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        unordered_map<char, int> m;
+        for(auto x: magazine){
+            m[x]++;
+        }
+        for(auto x: ransomNote){
+            if(!m[x]--){
+                return false;
+            }
+        }
+        return true;
+    }
+};
