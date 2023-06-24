c54b43b2ab267e803903cf2315e8cdfe6155dddf
Hardik Dhanjal <73985915+hardiktreats@users.noreply.github.com>
2023-06-24 21:25:17 +0530

    Added solution - LeetHub

---
+++ Floor in BST - GFG/floor-in-bst.cpp
@@ -0,0 +1,78 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct Node {
+    int data;
+    Node *right;
+    Node *left;
+
+    Node(int x) {
+        data = x;
+        right = NULL;
+        left = NULL;
+    }
+};
+
+int floor(Node *root, int x);
+
+Node *insert(Node *tree, int val) {
+    Node *temp = NULL;
+    if (tree == NULL) return new Node(val);
+
+    if (val < tree->data) {
+        tree->left = insert(tree->left, val);
+    } else if (val > tree->data) {
+        tree->right = insert(tree->right, val);
+    }
+
+    return tree;
+}
+
+int main() {
+    int T;
+    cin >> T;
+    while (T--) {
+        Node *root = NULL;
+
+        int N;
+        cin >> N;
+        for (int i = 0; i < N; i++) {
+            int k;
+            cin >> k;
+            root = insert(root, k);
+        }
+
+        int s;
+        cin >> s;
+
+        cout << floor(root, s) << "\n";
+    }
+}
+
+// } Driver Code Ends
+
+
+// Function to search a node in BST.
+
+int floor(Node* root, int input) {
+    if(root==NULL){
+        return -1;
+    }
+    int ans=-1;
+    while(root!=NULL){
+        int search = root->data;
+        if(search==input){
+            return search; 
+        }
+        else if(search>input){
+            root=root->left;
+        }
+        else{
+            ans=search;
+            root=root->right;
+        }
+    }
+    return ans;
+}
